dfc68e139f98655a2b01324f7b912b9f89b5b651
jborja-one <jborja.one@gmail.com>
2022-06-27 20:33:46 -0600

    fixed bug

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
-  // Pick a rondom quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(math.random() * apiQuotes.length)];
-  authorText.textContent = quote.author;
-  quoteText.textContent = quote.text;
+	// Pick a random quote from apiQuotes array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	authorText.textContent = quote.author;
+	quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function GetQuotes() {
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    // Catch Error Here
-  }
-  console.log(apiQuotes);
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// Catch Error Here
+		alert(error);
+	}
+	// console.log(apiQuotes);
 }
 
 // On Load
