828b0e91100d80fa567036618a70d0659754acda
David Borja <DBorja95@administrators-iMac.local>
2022-06-28 08:44:02 -0600

    more done

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author field is blandk and replace it with 'unknown'
@@ -22,12 +36,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function GetQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -47,7 +63,6 @@ function tweetQuote() {
 }
 
 //event listener
-twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 GetQuotes();
