5c15fadef4718db48212cb1053b985ebe01236cd
David Borja <DBorja95@administrators-iMac.local>
2022-06-28 08:27:26 -0600

    twitter button done

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
-	// Pick a random quote from apiQuotes array
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	authorText.textContent = quote.author;
-	quoteText.textContent = quote.text;
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if author field is blandk and replace it with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function GetQuotes() {
-	const apiUrl = 'https://type.fit/api/quotes';
-	try {
-		const response = await fetch(apiUrl);
-		apiQuotes = await response.json();
-		newQuote();
-	} catch (error) {
-		// Catch Error Here
-		alert(error);
-	}
-	// console.log(apiQuotes);
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    alert(error);
+  }
+  // console.log(apiQuotes);
 }
+
+// tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//event listener
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 GetQuotes();
